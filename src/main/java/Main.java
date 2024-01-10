8a0d5bfef3b2fed91a068f6edf701c96a83f1d80
Alexandr T <avers66@yandex.ru>
2024-01-10 20:29:44 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+/**
+ * Main
+ *
+ * @Author Tretyakov Alexandr
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(File.separator);
+    }
+}

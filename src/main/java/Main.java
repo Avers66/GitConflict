c341c742c0a53067af2289457574816639ab8f13
Alexandr T <avers66@yandex.ru>
2024-11-16 18:35:57 +0500

    //16.11.24 18:34 create string in local branch feature/test-2-commit-in-origin-develop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main {
         //10.11.24 20:46 My_feature
         //10.11.24 20:46 My_feature2 in github
         //16.11.24 18:07 create string in local branch feature/test-2-commit-in-origin-develop
+        //16.11.24 18:34 create string in local branch feature/test-2-commit-in-origin-develop
 
     }
 }

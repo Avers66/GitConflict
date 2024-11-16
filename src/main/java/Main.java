42703f34bc692ef6ce164156383abb777bb46fab
Avers66 <122222024+Avers66@users.noreply.github.com>
2024-11-16 18:06:10 +0500

    create new string in origin develop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         //comment1 (conflict) conflict2
         System.out.println(File.separator);
+        //create new string in origin develop
     
 
     }

0436e760d8f66ba2521b9d402f854de561d0e660
Avers66 <122222024+Avers66@users.noreply.github.com>
2024-11-16 18:05:07 +0500

    Delete-all-string-in-origin-develop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,14 +10,7 @@ public class Main {
     public static void main(String[] args) {
         //comment1 (conflict) conflict2
         System.out.println(File.separator);
-        //added comment 2 3
-        //added comment 3 2
-        //added comment 23
-        // 10.11.24 17:57
-        //10.11.24 18:02 github
-        //10.11.24 18:48 github
-        //10.11.24 18:55 github
-        //10.11.24 20:46 My_feature
-        //10.11.24 20:46 My_feature2 in github
+    
+
     }
 }
